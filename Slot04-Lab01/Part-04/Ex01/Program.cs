dfab00f5e3824dee733cc7123dda83f490c3e84c
NguyenHuyy0905 <huythuongtin@gmail.com>
2023-12-20 03:31:05 +0700

    Huy update

---
+++ Slot04-Lab01/Part-04/Ex01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the following information:");
+
+        // Input for name
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+
+        // Input for address
+        Console.Write("Address: ");
+        string address = Console.ReadLine();
+
+        // Input for phone
+        Console.Write("Phone: ");
+        string phone = Console.ReadLine();
+
+        // Display entered information
+        Console.WriteLine("\nDisplaying Information:");
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Address: {address}");
+        Console.WriteLine($"Phone: {phone}");
+
+        // Wait for user to press a key before closing the console
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
