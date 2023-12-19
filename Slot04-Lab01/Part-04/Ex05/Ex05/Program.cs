1ea39c037af22d2f580954ed617b7fbcda52bfc7
NguyenHuyy0905 <huythuongtin@gmail.com>
2023-12-20 03:40:04 +0700

    Huy update

---
+++ Slot04-Lab01/Part-04/Ex05/Ex05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Factorials of integers from 1 to 20:");
+
+        for (int i = 1; i <= 20; i++)
+        {
+            ulong factorial = CalculateFactorial(i);
+            Console.WriteLine($"Factorial of {i}: {factorial}");
+        }
+
+        // Wait for user to press a key before closing the console
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+
+    // Function to calculate the factorial of a number
+    static ulong CalculateFactorial(int n)
+    {
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+
+        ulong result = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            result *= (ulong)i;
+        }
+
+        return result;
+    }
+}
