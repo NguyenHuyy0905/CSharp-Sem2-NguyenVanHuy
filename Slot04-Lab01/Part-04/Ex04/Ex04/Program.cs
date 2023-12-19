83407599327028cbe8206852c77faa1a0eac1a40
NguyenHuyy0905 <huythuongtin@gmail.com>
2023-12-20 03:37:29 +0700

    Huy update

---
+++ Slot04-Lab01/Part-04/Ex04/Ex04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter an integer N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nThe first 9 multiples of {n} are:");
+
+        for (int i = 1; i <= 9; i++)
+        {
+            int multiple = n * i;
+            Console.WriteLine($"{n} x {i} = {multiple}");
+        }
+
+        // Wait for user to press a key before closing the console
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
